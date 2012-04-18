b6aff1d3a4be94fcced2138d7cfd68545d559405
Martin Backhaus <mrtn.bckhs@me.com>
2012-04-18 15:34:33 +0200

    english translation for boxes4ward

---
+++ languages/en/tl_boxes4ward_category.php
@@ -0,0 +1,40 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+
+/**
+ * Boxes4ward
+ * Contao extension to manage articles and contentelement in content boxes
+ *
+ * @copyright 4ward.media 2012 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ * @licence LGPL
+ * @filesource
+ * @package Boxes4ward
+ * @see https://github.com/psi-4ward/boxes4ward
+ */
+
+
+
+ /**
+  * Fields
+  */
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['name']			= array('Name', 'Please enter a name for the category.');
+
+
+ /**
+  * Buttons
+  */
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['new']				= array('New category', 'Create a new category.');
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['editheader']		= array('Edit category', 'Edit category ID %s');
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['copy']				= array('Copy category', 'Copy category ID %s');
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['delete']			= array('Delete category', 'Delete category ID %s');
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['show']				= array('Category details', 'Show up details of category ID %s');
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['edit']  			= array('Artikel bearbeiten', 'Edit the article of category ID %s');
+
+
+
+ /**
+  * Legends
+  */
+ $GLOBALS['TL_LANG']['tl_boxes4ward_category']['name_legend']		= 'Name';
+ 
---
+++ languages/en/tl_user.php
@@ -0,0 +1,19 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+
+/**
+ * Boxes4ward
+ * Contao extension to manage articles and contentelement in content boxes
+ *
+ * @copyright 4ward.media 2012 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ * @licence LGPL
+ * @filesource
+ * @package Boxes4ward
+ * @see https://github.com/psi-4ward/boxes4ward
+ */
+
+
+
+$GLOBALS['TL_LANG']['tl_user']['boxes4ward_legend'] = 'Contentboxes access rights';
+$GLOBALS['TL_LANG']['tl_user']['boxes4ward'] = array('Contentboxes categories','The users gain access to the selected contentboxes categories.');
