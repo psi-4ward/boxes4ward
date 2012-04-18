dc1ed9abea4228cab501a1e565e0a4c299567fac
Christoph Wiechert <wio@psitrax.de>
2012-04-18 14:44:55 +0200

    Added: Nice tl_content header with article- and modulename

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -19,8 +19,12 @@ if($this->Input->get('do') == 'boxes4ward')
 {
 	$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_boxes4ward_article';
 	
-// set news4wards checkPermissions function
-$GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = array('tl_content_boxes4ward', 'checkPermission');
+	// set news4wards checkPermissions function
+	$GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = array('tl_content_boxes4ward', 'checkPermission');
+
+	// set headerFields
+	$GLOBALS['TL_DCA']['tl_content']['list']['sorting']['headerFields'] = array('name');
+	$GLOBALS['TL_DCA']['tl_content']['list']['sorting']['header_callback'] = array('tl_content_boxes4ward','generateHeader');
 }
 
 class tl_content_boxes4ward extends Backend
@@ -33,6 +37,24 @@ class tl_content_boxes4ward extends Backend
 	{
 		parent::__construct();
 		$this->import('BackendUser', 'User');
+	}
+
+
+	/**
+	 * Add Modulname to headerFields
+	 * @param string $add
+	 * @param DataContainer $dc
+	 * @return array
+	 */
+	public function generateHeader($add, $dc)
+	{
+		$objModule = $this->Database->prepare('SELECT tl_module.name FROM tl_module
+												LEFT JOIN tl_boxes4ward_article ON (tl_module.id = tl_boxes4ward_article.module_id)
+												WHERE tl_boxes4ward_article.id=?')->execute(CURRENT_ID);
+
+		$add[$GLOBALS['TL_LANG']['tl_boxes4ward_article']['module_id'][0]] = $objModule->name;
+
+		return $add;
 	}
 
 	/**
