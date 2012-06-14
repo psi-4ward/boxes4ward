f50089ee029844b71c5a1f5e0f8a3af3ad22fb80
Christoph Wiechert <wio@psitrax.de>
2012-06-15 00:56:00 +0200

    Added: cssID property and first,last,even,odd as well as boxes4ward_article CSS classes. Thanks a lot to thescrat preparing the code

--- ModuleBoxes4ward.php
+++ ModuleBoxes4ward.php
@@ -61,6 +61,7 @@ class ModuleBoxes4ward extends Module
 
 		// filter articles to matching pages and generate its content elements
 		$strContent = '';
+		$intCounter = 1;
 		while($objArticle->next())
 		{
 			$objArticle->pages = deserialize($objArticle->pages,true);
@@ -85,15 +86,31 @@ class ModuleBoxes4ward extends Module
 				continue;
 			}
 
-			// add div with css id / class
-			if(strlen($objArticle->cssID[0]) || strlen($objArticle->cssID[1]))
+			// generate css classes (first,last,even,odd)
+			$objArticle->cssID = deserialize($objArticle->cssID);
+			$class = 'boxes4ward_article';
+			$class .= ($intCounter == 1) ? ' first' : '';
+			$class .= ($intCounter == $objArticle->numRows) ? ' last' : '';
+			$class .= ($intCounter%2) ? ' odd' : ' even';
+
+			// take the id/class from the article-attributes
+			$id = '';
+			if($objArticle->cssID)
 			{
-				$strContent .= '<div';
-				$objArticle->cssID[0] != '' ? $strContent .= ' id="'.$objArticle->cssID[0].'"' : '';
-				$objArticle->cssID[1] != '' ? $strContent .= ' class="ce_article '.$objArticle->cssID[1].'"' : '';
-				$strContent .= '>';
+				if(strlen($objArticle->cssID[0]))
+				{
+					$id = ' id="'.$objArticle->cssID[0].'"';
+				}
+
+				if(strlen($objArticle->cssID[1]))
+				{
+					$class .= ' '.$objArticle->cssID[1];
+				}
 			}
-			
+
+			// add a div holding css id / classes
+			$strContent .= sprintf('<div%s class="%s">',$id,$class);
+
 			// fetch content elements and generate it
 			$objCte = $this->Database->prepare("SELECT id FROM tl_content WHERE pid=?" . (!BE_USER_LOGGED_IN ? " AND invisible=''" : "") . " AND do='boxes4ward' ORDER BY sorting")
 									 ->execute($objArticle->id);
@@ -103,10 +120,9 @@ class ModuleBoxes4ward extends Module
 				$strContent .= $this->getContentElement($objCte->id);
 			}
 			
-			if(strlen($objArticle->cssID[0]) || strlen($objArticle->cssID[1]))
-			{
-				$strContent .= '</div>';
-			}			
+			$strContent .= '</div>';
+
+			$intCounter++;
 		}
 
 		$this->Template->content = $strContent;
